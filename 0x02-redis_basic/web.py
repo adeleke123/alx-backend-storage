b6597f5378307c3868ceb946391617d6b33de03c
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-05-13 22:07:00 +0100

    redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,55 +1,21 @@
 #!/usr/bin/env python3
 """
-Implementing an expiring web cache and tracker
+create a web cach
 """
-
 import redis
 import requests
-from typing import Callable
-from functools import wraps
-
-redis = redis.Redis()
+rc = redis.Redis()
+count = 0
 
 
-def wrap_requests(fn: Callable) -> Callable:
-    """Decorator that wraps d given func & provides caching functionality
-
-    Args:
-        fn: The function to be wrapped.
-
-    Returns:
-        The wrapped function
-    """
-
-    @wraps(fn)
-    def wrapper(url):
-        """Wrapper function that adds caching logic to the decorated func
-
-        Args:
-            url: The URL to fetch the page content from.
-
-        Returns:
-            The HTML content of the page
-        """
-        redis.incr(f"count:{url}")
-        cached_response = redis.get(f"cached:{url}")
-        if cached_response:
-            return cached_response.decode('utf-8')
-        result = fn(url)
-        redis.setex(f"cached:{url}", 10, result)
-        return result
-    return wrapper
-
-
-@wrap_requests
 def get_page(url: str) -> str:
-    """Fetches the HTML content of a web page.
+    """ get a page and cach value"""
+    rc.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    rc.incr(f"count:{url}")
+    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
+    return resp.text
 
-    Args:
-        url: The URL of the web page.
 
-    Returns:
-        The HTML content of the web page
-    """
-    response = requests.get(url)
-    return response.text
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
